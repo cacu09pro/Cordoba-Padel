8ab51be78485f77a2f96ad87e26f32756ff5cd03
cacu09pro <camilojosecattaneo@gmail.com>
2024-09-23 12:26:37 -0300

    Add files via upload

---
+++ Padel.js
@@ -0,0 +1,53 @@
+const home_torneo = [
+    {
+        Opt1: "Home",
+        Opt2: "Equipo",
+        Opt3: "Merch",
+    }
+]
+const filtro_torneo_zona = [
+    {
+        zona: "Norte",
+        zona: "Sur",
+        zona: "Centro",
+    }
+]
+const filtro_torneo_fecha = [
+    {
+    }
+]
+const home_equipo = [
+    {
+        Opt1: "Home",
+        Opt2: "Torneo",
+        Opt3: "Merch",
+    }
+]
+const filtro_equipo_palas = [
+    {
+        marca: "Adidas",
+        marca: "Nox",
+        marca: "Puma",
+    }
+]
+const filtro_equipo_pelotas = [
+    {
+        marca: "Wilson",
+        marca: "Bullpadel",
+        marca: "Odea",
+    }
+]
+const home_merch = [
+    {
+        Opt1: "Home",
+        Opt2: "Torneo",
+        Opt3: "Equipo",
+    }
+]
+const filtro_merch = [
+    {
+        marca: "Adidas",
+        marca: "Nike",
+        marca: "Puma",
+    }
+]
